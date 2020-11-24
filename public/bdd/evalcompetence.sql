8b494b7b48db474346f9f236a49bc2b1f39220b3
Philippe NAVEILHAN <travail@naveilhan.com>
2020-11-24 10:41:03 +0100

    add bdd values

---
+++ public/bdd/evalcompetence.sql
@@ -0,0 +1,7 @@
+INSERT INTO matools.evalcompetence (id, bloc_id, name, evaluation_id) VALUES (76, 1, 'Réciter l''alphabet en rôtant', 33);
+INSERT INTO matools.evalcompetence (id, bloc_id, name, evaluation_id) VALUES (77, 1, 'Réciter l''alphabet en pétant', 33);
+INSERT INTO matools.evalcompetence (id, bloc_id, name, evaluation_id) VALUES (78, 2, 'Ecrire la graphie "A" dans son slip', 33);
+INSERT INTO matools.evalcompetence (id, bloc_id, name, evaluation_id) VALUES (79, 2, 'Ecrire l''alphabet avec ses crottes de nez', 33);
+INSERT INTO matools.evalcompetence (id, bloc_id, name, evaluation_id) VALUES (80, 3, 'Mettre son caca sur les murs', 33);
+INSERT INTO matools.evalcompetence (id, bloc_id, name, evaluation_id) VALUES (91, 1, 'Compétence 1', 40);
+INSERT INTO matools.evalcompetence (id, bloc_id, name, evaluation_id) VALUES (142, 1, 'reconnaître et tracer les nombres de 1 à 7', 44);
---
+++ public/bdd/evaltheme.sql
@@ -0,0 +1,6 @@
+INSERT INTO matools.evaltheme (id, name) VALUES (1, 'Mobiliser le langage dans toutes ses dimensions');
+INSERT INTO matools.evaltheme (id, name) VALUES (2, 'Agir, s''exprimer, comprendre à travers l''activité physique');
+INSERT INTO matools.evaltheme (id, name) VALUES (3, 'Agir, s''exprimer, comprendre à travers les activités artistiques');
+INSERT INTO matools.evaltheme (id, name) VALUES (4, 'Construire les premiers outils pour structurer sa pensée');
+INSERT INTO matools.evaltheme (id, name) VALUES (5, 'Explorer le monde');
+INSERT INTO matools.evaltheme (id, name) VALUES (6, 'Apprendre ensemble et vivre ensemble');
