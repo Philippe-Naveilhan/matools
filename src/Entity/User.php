7dc5e536c561ad48429d03df3676d6889dc6c435
Philippe NAVEILHAN <travail@naveilhan.com>
2020-11-25 15:14:51 +0100

    creation compte

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,10 +7,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
  */
 class User implements UserInterface
 {
@@ -67,6 +69,11 @@ class User implements UserInterface
      * @ORM\OneToMany(targetEntity=Evaluation::class, mappedBy="teacher")
      */
     private $evaluations;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -251,6 +258,18 @@ class User implements UserInterface
                 $evaluation->setTeacher(null);
             }
         }
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
