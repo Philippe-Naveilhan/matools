2ee51b6ad3893b2107426e0631c38ce159075810
Philippe NAVEILHAN <travail@naveilhan.com>
2020-11-28 00:28:57 +0100

    domain & cascade ok

---
+++ src/Data/matools_evalcategory.sql
@@ -0,0 +1,7 @@
+INSERT INTO matools.evalcategory (id, theme_id, name) VALUES (1, 1, 'L''oral');
+INSERT INTO matools.evalcategory (id, theme_id, name) VALUES (2, 1, 'L''écrit');
+INSERT INTO matools.evalcategory (id, theme_id, name) VALUES (3, 2, 'unique');
+INSERT INTO matools.evalcategory (id, theme_id, name) VALUES (4, 3, 'unique');
+INSERT INTO matools.evalcategory (id, theme_id, name) VALUES (5, 4, 'unique');
+INSERT INTO matools.evalcategory (id, theme_id, name) VALUES (6, 5, 'unique');
+INSERT INTO matools.evalcategory (id, theme_id, name) VALUES (7, 6, 'unique');
---
+++ src/Data/matools_level.sql
@@ -0,0 +1,3 @@
+INSERT INTO matools.level (id, name) VALUES (1, 'PS');
+INSERT INTO matools.level (id, name) VALUES (2, 'MS');
+INSERT INTO matools.level (id, name) VALUES (3, 'GS');
