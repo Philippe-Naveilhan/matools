81e0d0ee2676340b3e0042e3681c77fd243b7651
Philippe NAVEILHAN <travail@naveilhan.com>
2021-04-14 09:59:09 +0200

    fix matools_fr.crt

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -23,31 +23,23 @@ class HomeController extends AbstractController
     public function portrait(): Response
     {
         $titles = [
-            ['title'=>'Livres','color'=>'red','text'=>'1984, Orwell ou MAUS de ???'],
-            ['title'=>'Musique','color'=>'blue','text'=>'Jazz, Blue, Folk, Chansonniers, etc...'],
-            ['title'=>'Films','color'=>'grey','text'=>'Amadeus, Orange Mécanique, Musée haut - Musée bas'],
-            ['title'=>'Couleurs','color'=>'green','text'=>'blanc cassé'],
-            ['title'=>'Animal','color'=>'yellow','text'=>'Belette ou loutre. Un animal joueur.'],
-            ['title'=>'Langage','color'=>'red','text'=>'PHP Javascript'],
-            ['title'=>'Transport','color'=>'blue','text'=>'Mon esprit'],
-            ['title'=>'Plante','color'=>'purple','text'=>'Chanvre'],
-            ['title'=>'Vêtement','color'=>'whitesmoke','text'=>'Des chaussons ?'],
-            ['title'=>'Métier','color'=>'darkblue','text'=>'Développeur aujourd\'hui... demain ?'],
-            ['title'=>'Sport','color'=>'red','text'=>'Les échecs, rien de plus physique en tous cas...'],
-            ['title'=>'Odeur','color'=>'yellow','text'=>'L\'herbe, la bouse de vache, la campagne...'],
-            ['title'=>'Pays','color'=>'green','text'=>'La notion de frontières m\'est assez étrangère...'],
+            ['title'=>'Livres','img'=>'red1.jpg','color'=>'red','text'=>'1984 d\'Orwell,<br> MAUS de Spiegelman'],
+            ['title'=>'Musique','img'=>'green1.jpg','color'=>'green','text'=>'De Brassens à Shakaponk en passant par Mozart, Debussy, Beethoven'],
+            ['title'=>'Films','img'=>'yellow1.jpg','color'=>'grey','text'=>'Amadeus, Orange Mécanique, Musée haut - Musée bas'],
+            ['title'=>'Couleurs','img'=>'orange1.jpg','color'=>'orange','text'=>'blanc cassé'],
+            ['title'=>'Animal','img'=>'red2.jpg','color'=>'red','text'=>'Belette ou loutre. Un animal joueur.'],
+            ['title'=>'Langage','img'=>'green2.jpg','color'=>'green','text'=>'PHP Javascript'],
+            ['title'=>'Transport','img'=>'yellow2.jpg','color'=>'yellow','text'=>'Mon esprit'],
+            ['title'=>'Plante','img'=>'red3.jpg','color'=>'red','text'=>'Chanvre'],
+            ['title'=>'Vêtement','img'=>'green3.jpg','color'=>'green','text'=>'Des chaussons ?'],
+            ['title'=>'Métier','img'=>'red1.jpg','color'=>'red','text'=>'Développeur aujourd\'hui... demain ?'],
+            ['title'=>'Sport','img'=>'orange1.jpg','color'=>'orange','text'=>'Les échecs, rien de plus physique en tous cas...'],
+            ['title'=>'Odeur','img'=>'yellow1.jpg','color'=>'grey','text'=>'L\'herbe, la bouse de vache, la campagne...'],
+            ['title'=>'Pays','img'=>'red3.jpg','color'=>'red','text'=>'La notion de frontières m\'est assez étrangère...'],
         ];
-        $bg = [];
-        if ($dossier = opendir('build/otero')) {
-            while(false !== ($fichier = readdir($dossier))){
-                if($fichier != '.' && $fichier != '..') {
-                    $bg[]=$fichier;
-                }
-            }
-        };
+
         return $this->render('CV/index.html.twig', [
-            'titles'=> $titles,
-            'bg'=>$bg,
+            'titles'=> $titles
         ]);
     }
 }
